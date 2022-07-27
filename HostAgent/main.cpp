acdbe57ebbd9758195268773daea04cd601ba274
George G <grreulvi@gmail.com>
2022-07-28 01:29:51 +0300

    [hostagent] [targetagent] [network] work in progresss....

--- HostAgent/main.cpp
+++ HostAgent/main.cpp
@@ -2,19 +2,93 @@
 #define BOOST_ASIO_NO_DEPRECATED
 #define BOOST_ASIO_NO_TS_EXECUTORS
 #include "boost/asio.hpp"
-//#include "boost/asio/impl/src.hpp"
+#include "boost/bind.hpp"
+#include "boost/array.hpp"
+
+#include "..\common\DeploykaNet.h"
 
 using namespace boost::asio;
 
+/******************************************************************************//*
+*
+Printer class usage:
 int main() {
-  std::cout << "Hello from Host Agent!" << std::endl;
   io_context context;
-  ip::tcp::endpoint ep(ip::make_address_v4("127.0.0.1"), 2001);
-  ip::tcp::socket sock(context);
-  sock.connect(ep);
+  boost::system::error_code ec;
 
-  //sock.send(
+  Printer printer(context);
 
+  printer.printFunc(ec);
+
+  context.run();
+}
+*********************************************************************************/
+class Printer {
+  int m_count;
+  steady_timer m_timer;
+
+public:
+  Printer(io_context & ctx)
+    : m_count(0)
+    , m_timer(ctx, chrono::seconds(1))
+  {}
+
+  ~Printer() {
+    std::cout << "\ncount: " << m_count << std::endl;
+  }
+
+  void printFunc(boost::system::error_code const & ec) {
+    if (ec.value() != 0) {
+      std::cout << "error: " << ec.value() << "\n";
+    }
+    if (m_count < 5) {
+      std::cout << this->m_count << " ";
+      m_count += 1;
+      m_timer.expires_at(this->m_timer.expiry() + chrono::seconds(1));
+      m_timer.async_wait(boost::bind(&Printer::printFunc, this, placeholders::error));
+    }
+  }
+};
+
+int main(int argc, char* argv[]) {
+  std::ostream::sync_with_stdio(false);
+
+  if (argc != 2) {
+    std::cout << "Provide hostname\n";
+    return -1;
+  }
+
+  io_context context;
+
+  ip::tcp::resolver resolver(context);
+
+  try {
+    ip::tcp::resolver::results_type gaiResults = resolver.resolve(argv[1], "daytime");
+
+    ip::tcp::socket sock(context);
+
+    connect(sock, gaiResults);
+
+    for (;;) {
+      boost::array<char, 128> buf;
+      boost::system::error_code ec;
+
+      size_t len = sock.read_some(buffer(buf), ec);
+
+      if (ec == boost::asio::error::eof) {
+        break;
+      }
+      else if (ec) {
+        throw boost::system::system_error(ec);
+      }
+
+      std::cout.write(buf.data(), len);
+
+    }
+  }
+  catch (std::exception & e) {
+    std::cerr << e.what() << std::endl;
+  }
 
   return 0;
 }
--- TargetAgent/main.cpp
+++ TargetAgent/main.cpp
@@ -4,7 +4,79 @@
 #include "boost/thread.hpp"
 #include "boost/asio.hpp"
 #include "boost/bind.hpp"
+
+#include "..\common\DeploykaNet.h"
 //#include "boost/asio/impl/src.hpp"
+
+class DeploykaTcpConnection: boost::enable_shared_from_this<DeploykaTcpConnection> {
+  boost::asio::ip::tcp::socket m_sock;
+
+  DeploykaTcpConnection(boost::asio::io_context& ctx)
+    : m_sock(ctx)
+  {
+    m_msg.m_size = -1;
+    m_msg.m_type = Deployka::DMT_Null;
+  }
+
+  std::array<unsigned char, 8196> m_arrayBuffer;
+
+  Deployka::Message m_msg;
+
+public:
+  typedef boost::shared_ptr<DeploykaTcpConnection> pointer;
+
+  boost::asio::ip::tcp::socket & socket() { return m_sock; }
+
+  static pointer create(boost::asio::io_context& ctx) {
+    pointer result = boost::make_shared<DeploykaTcpConnection>(ctx);
+    return result;
+  }
+
+  void start() {
+    // TODO
+    m_sock.async_receive(boost::asio::buffer(m_arrayBuffer),
+      boost::bind(DeploykaTcpConnection::handleRead, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+  }
+
+  void handleRead(boost::system::error_code const & ec, size_t received) {
+    if (m_msg.m_size < 0) {
+      if (received >= sizeof(Deployka::Message::m_size)) {
+        int size = 0;
+        std::memcpy(&size, m_arrayBuffer.data(), 4);
+      }
+    }
+  }
+
+  void handleWrite() {
+  }
+};
+
+class DeploykaTcpServer {
+  boost::asio::io_context& m_ctx;
+  boost::asio::ip::tcp::acceptor m_acceptor;
+public:
+  DeploykaTcpServer(boost::asio::io_context& ctx)
+    : m_ctx(ctx)
+    , m_acceptor(ctx, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), Deployka::TCP_PORT))
+  {
+    startAccept();
+  }
+
+  void handleAccept(DeploykaTcpConnection* newConnection, boost::system::error_code const & ec) {
+    if (!ec) {
+      newConnection->start();
+    }
+    startAccept();
+  }
+
+  void startAccept() {
+    DeploykaTcpConnection::pointer newConnection = DeploykaTcpConnection::create(m_ctx);
+
+    m_acceptor.async_accept(newConnection->socket(),
+      boost::bind(DeploykaTcpServer::handleAccept, this, newConnection, boost::asio::placeholders::error));
+  }
+
+};
 
 
 using namespace boost::asio;
