b6ef30d5aa3a2312302e05616e555e583d01c217
George G <grreulvi@gmail.com>
2022-08-30 01:00:06 +0300

    [CollectArtifacts] * move stream output operators under std namespace to make them available to boost program options * move XML parsing code to corresponding function * add default values to all command line parameters * add search for requested targets in processed targets * add variables substitution for processing targets

--- CollectArtifacts/main.cpp
+++ CollectArtifacts/main.cpp
@@ -23,20 +23,21 @@ enum DependencyType : unsigned int {
 };
 
 struct ArtifactDependency {
-  DependencyType type;
-  std::string path;
-  std::string pattern;
+  DependencyType type; ///< type of dependency
+  std::string path; ///< path to file or path to folder or root directory to apply search by pattern
+  std::string pattern; ///< pattern to search
 };
 
 struct Artifact {
-  std::string name;
-  std::string pathToDir;
-  std::vector<ArtifactDependency> dependencies;
+  std::string name; ///< unique name
+  std::string pathToDir; ///< where to store collected artifact
+  std::vector<ArtifactDependency> dependencies; ///< files that must be deployed along with target
 };
 }
 
-template <class T>
-std::ostream& operator << (std::ostream& os, std::vector<T> const& vec) {
+namespace std {
+  template <class T>
+  std::ostream& operator << (std::ostream& os, std::vector<T> const& vec) {
 
     if (!vec.empty()) {
       os << vec.front();
@@ -47,35 +48,36 @@ std::ostream& operator << (std::ostream& os, std::vector<T> const& vec) {
     }
 
     return os;
-}
-
-std::ostream& operator << (std::ostream& os, Deployka::ArtifactDependency const& aDependency) {
-  switch (aDependency.type) {
-  case Deployka::DDT_directory:
-    os << "Directory: " << aDependency.path;
-    break;
-  case Deployka::DDT_file:
-    os << "File: " << aDependency.path;
-    break;
-  case Deployka::DDT_pattern:
-    os << "Pattern: " << aDependency.path << '/' << aDependency.pattern;
-    break;
-  default:
-    os << "UNKNOWN (Type: " << aDependency.type << "path: " << aDependency.path << "; pattern: " << aDependency.pattern << ')';
-    break;
   }
-  return os;
+
+  std::ostream& operator << (std::ostream& os, Deployka::ArtifactDependency const& aDependency) {
+    switch (aDependency.type) {
+    case Deployka::DDT_directory:
+      os << "Directory: " << aDependency.path;
+      break;
+    case Deployka::DDT_file:
+      os << "File: " << aDependency.path;
+      break;
+    case Deployka::DDT_pattern:
+      os << "Pattern: " << aDependency.path << '/' << aDependency.pattern;
+      break;
+    default:
+      os << "UNKNOWN (Type: " << aDependency.type << "path: " << aDependency.path << "; pattern: " << aDependency.pattern << ')';
+      break;
+    }
+    return os;
+  }
+
+  std::ostream& operator << (std::ostream& os, Deployka::Artifact const& aTarget) {
+    os << aTarget.name << " (" << aTarget.pathToDir << ")\n";
+    os << "\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n";
+    os << aTarget.dependencies;
+    os << "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
+    return os;
+  }
+
 }
 
-std::ostream& operator << (std::ostream& os, Deployka::Artifact const& aTarget) {
-  os << aTarget.name << " (" << aTarget.pathToDir << ")\n";
-  os << "\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n";
-  os << aTarget.dependencies;
-  os << "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
-  return os;
-}
-
-std::vector<Deployka::Artifact> g_targets;
 
 // Опции:
 // 1) путь к папке с конфигом
@@ -156,6 +158,11 @@ R"str(<Dependency>$SrcRoot\VI\WebClient\Backend\WebServer\$Platform\$Config\WebS
   std::cout << "result: " << res;
 }
 
+/*!
+ * @brief apply lowercase to each char in string
+ * @param [in] in string to lowercase
+ * @return same string but lowercased
+*/
 std::string & str_to_lower(std::string & in) {
   std::transform(in.begin(), in.end(), in.begin(), [](char c) { return (char)std::tolower(c); });
   return in;
@@ -164,6 +171,9 @@ std::string & str_to_lower(std::string & in) {
 /*!
 * @brief substitudes the value of the variable where the name of the variable occurs with leading '$'
 *        and reparses text inside $(...) (to separate variable names with plain text $($arch)bit => 64bit)
+* @param[in] varsHash vars and their values
+* @param[in] inStr string to substitude to
+* @return string with all found variables substituted
 */
 std::string substVars(std::map<std::string, std::string> & varsHash, std::string & inStr) {
 
@@ -196,7 +206,7 @@ std::string substVars(std::map<std::string, std::string> & varsHash, std::string
     if (suffix.length() > 0) {
       result.append(suffix.first, suffix.second);
       inStr = std::move(result);
-      result.resize(0);
+      result.clear();
       result.reserve(inStr.length());
     }
     std::smatch tmp;
@@ -219,8 +229,7 @@ std::string substVars(std::map<std::string, std::string> & varsHash, std::string
     str_to_lower(tmp);
 
     if (!varsHash.count(tmp)) {
-
-      std::cout << "Cannot find item " << tmp << " in hash\n";
+      std::cout << "[W] Cannot find item " << tmp << " in hash\n";
       result.append(item.first, item.second);
       continue;
     }
@@ -233,91 +242,33 @@ std::string substVars(std::map<std::string, std::string> & varsHash, std::string
   return result;
 }
 
-
-
-int main(int argc, char * argv[]) {
-
-  std::ios::sync_with_stdio(false);
-
-#ifdef _TESTS_
-  argc;
-  argv;
-  //TEST_regex();
-  TEST_substVars();
-#else
-  std::string configFile;
-
-  try {
-  po::options_description desc("Collect Artifacts command line arguments");
-  desc.add_options()
-    ("help", "print help message")
-    ("variant,V",   po::value<std::string>(),              "debug or release")
-    ("mem-model,M", po::value<int>(),                      "32 or 64")
-    ("config,c",    po::value<std::string>(&configFile)->default_value("./BuildArtifacts.xml"), "path to artifacts description file")
-    ("src-root,r",  po::value<std::string>(),              "Path to directory that will be substituted as SrcRoot in artifacts description")
-    ("target,T",    po::value<std::vector<std::string>>(), "Which target from config to collected")
-  ;
-
-  po::positional_options_description posOpts;
-  posOpts.add("target", -1);
-
-  po::variables_map vm;
-  po::store(
-    po::command_line_parser(argc, argv)
-      .options(desc)
-      .positional(posOpts)
-      .run(),
-    vm);
-  po::notify(vm);
-
-  if (vm.count("help")) {
-    std::cout << desc << std::endl;
-    return 0;
-  }
-
-  if (vm.count("variant")) {
-    std::cout << "variant:     " << vm["variant"].as<std::string>() << std::endl;
-  }
-
-  if (vm.count("mem-model")) {
-    std::cout << "mem model:   " << vm["mem-model"].as<int>() << std::endl;
-  }
-
-  if (vm.count("config")) {
-    std::cout << "config path: " << vm["config"].as<std::string>() << std::endl;
-  }
-
-  if (vm.count("src-root")) {
-    std::cout << "source root: " << vm["src-root"].as<std::string>() << std::endl;
-  }
-
-  if (vm.count("target")) {
-    std::vector<std::string> vec = vm["target"].as<std::vector<std::string>>();
-
-    //std::cout << "targets:     " << vec << std::endl;
-  }
-
-  /// LOAD XML and parse targets
-
-  pt::ptree aTree;
-
-  pt::read_xml(configFile, aTree);
-
+/*!
+ * @brief fill targets info from XML
+ * @param[in] configFile path to XML file
+ * @return vector of targets filled from XML file
+*/
+std::vector<Deployka::Artifact> loadTargetsFromXML(std::string const & configFile) {
+//
+  std::vector<Deployka::Artifact> result;
+  pt::ptree aTree; //
+ //
+  pt::read_xml(configFile, aTree); //
+//
   for (pt::ptree::value_type & targetNode : aTree.get_child("Root.Targets")) {
-
+//
     Deployka::Artifact aTarget;
-
+//
     aTarget.name = targetNode.second.get<std::string>("Name");
     aTarget.pathToDir = targetNode.second.get<std::string>("PathToDirectory");
     for (pt::ptree::value_type& dependencyNode : targetNode.second.get_child("Dependencies")) {
-
+//
       Deployka::ArtifactDependency aDependency;
       aDependency.type = Deployka::DDT_none;
-
+//
       if (dependencyNode.second.count("<xmlattr>")) {
-
+//
         pt::ptree::value_type & xmlAttrVal = dependencyNode.second.get_child("<xmlattr>").front();
-
+//
         if (xmlAttrVal.first == "type" && xmlAttrVal.second.data() == "pattern") {
           std::string rootDirStr = dependencyNode.second.get<std::string>("RootDir");
           std::string patternStr = dependencyNode.second.get<std::string>("Pattern");
@@ -345,17 +296,135 @@ int main(int argc, char * argv[]) {
           aDependency.type = Deployka::DDT_file;
           aDependency.path = dependencyNode.second.data();
       }
-
+//
       if (dependencyNode.first == "Dependency" && aDependency.type != Deployka::DDT_none) {
         aTarget.dependencies.emplace_back(aDependency);
       }
     }
+//
+    result.emplace_back(std::move(aTarget));
+  }
+//
+  return result;
+}
 
-    //std::cout << aTarget << '\n';
-    g_targets.emplace_back(std::move(aTarget));
+//================================================================================
+int main(int argc, char * argv[]) {
+
+  std::ios::sync_with_stdio(false);
+
+#ifdef _TESTS_
+  argc;
+  argv;
+  //TEST_regex();
+  TEST_substVars();
+#else
+  std::string configFile;
+  std::vector<std::string> requestedTargets;
+
+  try {
+  po::options_description desc("Collect Artifacts command line arguments");
+  desc.add_options()
+    ("help", "print help message")
+    (
+      "variant,V",   
+      po::value<std::string>()->default_value("Debug"),
+      "debug or release"
+    )
+    (
+      "mem-model,M", 
+      po::value<int>()->default_value(64),
+      "32 or 64"
+    )
+    (
+      "src-root,r",
+      po::value<std::string>()->default_value("./"),
+      "Path to directory that will be substituted as SrcRoot in artifacts description"
+    )
+    (
+      "config,c",
+      po::value<std::string>(&configFile)->default_value("./BuildArtifacts.xml"),
+      "path to artifacts description file"
+    )
+    (
+      "target,T",
+      po::value<std::vector<std::string>>(&requestedTargets)->default_value({"Frontend", "WebServer"}),
+      "Which target from config to collected"
+    )
+  ;
+
+  po::positional_options_description posOpts;
+  posOpts.add("target", -1);
+
+  po::variables_map vm;
+  po::store(
+    po::command_line_parser(argc, argv)
+      .options(desc)
+      .positional(posOpts)
+      .run(),
+    vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << desc << std::endl;
+    return 0;
   }
 
-  std::cout << g_targets;
+  if (vm.count("variant")) {
+    std::cout << "variant:     " << vm["variant"].as<std::string>() << std::endl;
+  }
+
+  if (vm.count("mem-model")) {
+    std::cout << "mem model:   " << vm["mem-model"].as<int>() << std::endl;
+  }
+
+  if (vm.count("src-root")) {
+    std::cout << "source root: " << vm["src-root"].as<std::string>() << std::endl;
+  }
+
+  if (vm.count("config")) {
+    std::cout << "config path: " << vm["config"].as<std::string>() << std::endl;
+  }
+
+  if (vm.count("target")) {
+    std::cout << "targets:     " << vm["target"].as<std::vector<std::string>>() << std::endl;
+  }
+
+  std::vector<Deployka::Artifact> targets = loadTargetsFromXML(configFile);
+  std::vector<Deployka::Artifact> toProcess;
+//
+  for (std::string const& target : requestedTargets) {
+    auto foundIt = std::find_if(targets.begin(), targets.end(), [&target](Deployka::Artifact artifact) { return artifact.name == target; });
+    if (foundIt == targets.end()) {
+      std::cout << "[W] Requested target " << target << " not found in XML\n";
+      continue;
+    } //
+    toProcess.push_back(*foundIt);
+  } //
+//
+  std::cout << "targets to process:\n" << toProcess << '\n';
+//
+  // TODO load settings from same XML config file
+  std::map<std::string, std::string> varsHash = {
+    {"platform", std::to_string(vm["mem-model"].as<int>())},
+    {"srcroot", vm["src-root"].as<std::string>()},
+    {"outroot", R"-(E:\files\OutRoot)-"},
+    {"config", vm["variant"].as<std::string>()}
+  };
+//
+  for (Deployka::Artifact& target : toProcess) {
+//
+    target.pathToDir = substVars(varsHash, target.pathToDir);
+//
+    for (Deployka::ArtifactDependency& dependency : target.dependencies) {
+      dependency.path = substVars(varsHash, dependency.path);
+      if (!dependency.pattern.empty()) {
+        dependency.pattern = substVars(varsHash, dependency.pattern);
+      }
+    }
+  }
+//
+  std::cout << "after subst vars: \n" << toProcess << '\n';
 
   } // /TRY
   catch(std::exception& e) {
@@ -364,10 +433,9 @@ int main(int argc, char * argv[]) {
   }
   catch(...) {
       std::cerr << "Exception of unknown type!\n";
+      return 2;
   }
 #endif
-
-
 
   return 0;
 }
